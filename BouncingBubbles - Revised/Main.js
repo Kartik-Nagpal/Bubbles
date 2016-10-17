c5c92bd01755f3894b7983fa2ced1a98d22e5536
LightningLord4 <LightningLord4@users.noreply.github.com>
2016-10-17 11:55:54 -0500

    The Whole Project
    Yay! I'm putting another project on my website!

---
+++ BouncingBubbles - Revised/Main.js
@@ -0,0 +1,15 @@
+var myName = ["Couloumbic", "Force"];
+
+var yellow = [50, 100, 50];
+var black = [0, 0, 0];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [yellow, black, green];
+
+//bubbleShape = 'square';
+//bubbleShape = 'circle';
+bubbleShape = 'triangle';
+
+drawName(myName, letterColors);
+bounceBubbles();
