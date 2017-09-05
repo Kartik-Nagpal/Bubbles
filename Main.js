78b8c5f4897e5b0ed8ede72f1862a48d8358e381
Kartik-Nagpal <kartik-nagpal@g.austincc.edu>
2017-09-04 20:57:49 -0500

    Forgot to account for asynchronous compilation. Fixed message collection error.

--- Main.js
+++ Main.js
@@ -3,11 +3,14 @@ var message = "Hello There!";
 function choose()
 {
   var URL = (window.location.href).split("#");
-  var info = (URL[URL.length]).split("!");
+  console.log(URL);
+  var info = (URL[URL.length - 1]).split("!");
+  console.log(info);
   var num = parseInt(info[0]);
-  if(info[info.length] != null && info[info.length] != num)
+  if(info[info.length - 1] != null && info[info.length - 1] != num)
   {
-     message = info[info.length];
+     message = info[info.length - 1];
+     console.log(message);
   }
 
   if(num != null && num > 0 && num <= 5)
@@ -16,7 +19,7 @@ function choose()
   }
   var input = parseInt(prompt("Please Choose a Type(enter the number):\n1. Circle\n2. Square\n3. Triangle\n4. Hexagon\n5. Cross", "1"));
 
-  if(input != null && input > 0 && input <= 5)
+   if(input != null && input > 0 && input <= 5)
 	{
     return input - 1;
 	}
@@ -26,7 +29,7 @@ function choose()
 	}
 }
 
-
+var c = choose();
 var myName = [message, "LightningLord Productions"];
 
 var yellow = [50, 100, 50];
@@ -35,8 +38,6 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 var letterColors = [yellow, black, green, blue, purple];
-
-var c = choose();
 
 Shapes = ['circle', 'square', 'triangle', 'hexagon', 'cross'];
 bubbleShape = Shapes[c];
